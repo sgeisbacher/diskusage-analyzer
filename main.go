2b07177faa61bc36c20de9a5d192f7fb6d683218
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2017-03-01 22:49:01 +0100

    make added dirhotspots to analyzing-process

--- main.go
+++ main.go
@@ -10,30 +10,32 @@ import (
 const TOP_FILES int = 10
 
 var fileHotspots FileInfos
-var dirStore DirInfos
+var dirHotspotsCtx *DirHotspotsContext
 
 func visit(path string, f os.FileInfo, err error) error {
 	if !f.IsDir() {
 		fileInfo := FileInfo{path, f.Size()}
 		fileHotspots.Add(fileInfo)
-		dirStore.Add(fileInfo)
+		dirHotspotsCtx.Add(fileInfo)
 	}
 	return nil
 }
 
 func main() {
 	fileHotspots = make(FileInfos, TOP_FILES)
-	dirStore = make(DirInfos)
+	dirHotspotsCtx = &DirHotspotsContext{
+		dirInfos:   DirInfos{},
+		dirInfoIdx: DirInfoIdx{},
+	}
+
 	flag.Parse()
 	root := flag.Arg(0)
+
 	fmt.Println("collecting infos ...")
 	filepath.Walk(root, visit)
+
 	fmt.Println("analyzing ...")
 	fmt.Printf("file-hotspots:\n%v\n", fileHotspots)
-	fmt.Printf("directory-hotspots:\n  <not yet implemented>\n\n")
+	fmt.Printf("directory-hotspots:\n%v\n", dirHotspotsCtx.GetHotspots(TOP_FILES))
 	fmt.Printf("tree-hotspots:\n  <not yet implemented>\n\n")
-	fmt.Println("debug:")
-	for _, val := range dirStore {
-		fmt.Printf("%v\n", val)
-	}
 }
